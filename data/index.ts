60bc0f9adfd7c170d05ab332c7a8166ea29aced1
FullStackSimon <fullstacksimon@gmail.com>
2024-08-16 13:51:22 +0200

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+export const alkis = [
+  { name: "Alex", counter: 0 },
+  { name: "Sims", counter: 0 },
+  { name: "Simon", counter: 0 },
+  { name: "Chris", counter: 0 },
+  { name: "Maria", counter: 0 },
+];
