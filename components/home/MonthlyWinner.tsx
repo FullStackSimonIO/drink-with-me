47e947956323a6b53b9914b006e8931178fcc699
FullStackSimon <fullstacksimon@gmail.com>
2024-08-20 12:27:56 +0200

    added hero

--- components/home/MonthlyWinner.tsx
+++ components/home/MonthlyWinner.tsx
@@ -7,7 +7,7 @@ const MonthlyWinner = () => {
     <div className="flex flex-col pt-10 items-center h-screen text-3xl font-bold background-light800_dark300">
       <div className="flex flex-col items-center justify-center text-center">
         <h2 className="mb-6 dark:text-white text-dark-100">
-          Aktueller Alkoholiker des Monats:..
+          Aktueller Alkoholiker des Monats:..w
         </h2>
         <Card />
       </div>
